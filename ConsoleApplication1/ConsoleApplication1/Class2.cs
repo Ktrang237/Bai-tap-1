a0f1a3543deda6527472accbe05e1d23402c3575
Ktrang237 <ktrang2307@gmail.com>
2016-11-16 22:53:10 +0700

    Bai tap 1

---
+++ ConsoleApplication1/ConsoleApplication1/Class2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Rapchieuphim : Program
+    {
+    }
+
+    public class Program
+    {
+    }
+
+    public class phim
+    {
+        public Rapchieuphim Rapchieuphim
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+    }
+
+    public class Vexemphim : Rapchieuphim
+    {
+    }
+
+    public class Nguoiquanly : Program
+    {
+    }
+
+    public class VeThuong : Vexemphim
+    {
+    }
+
+    public class VeVip : Vexemphim
+    {
+    }
+}
